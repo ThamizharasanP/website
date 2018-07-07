64a9c288d5c124113697428528d376c6abcf551b
Sakthi <sakthi@3964@gmail.com>
2018-07-08 02:48:35 +0530

    custom banner image added

--- src/app/directive/custom-page-slider/custom-page-slider.component.ts
+++ src/app/directive/custom-page-slider/custom-page-slider.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-page-slider.component.scss']
 })
 export class CustomPageSliderComponent implements OnInit {
-
+  bannerImg: any;
   constructor() { }
 
   ngOnInit() {
+    this.homeBannerImg();
+  }
+
+  homeBannerImg() {
+    this.bannerImg = [
+      {
+        'img': 'assets/image/home/banner-image/crd-2017.jpg'
+      },
+      {
+        'img': 'assets/image/home/banner-image/girls-lead-girls.jpg'
+      },
+      {
+        'img': 'assets/image/home/banner-image/iyd-2018.png'
+      },
+      {
+        'img': 'assets/image/home/banner-image/summer-camp-2018.jpg'
+      },
+      {
+        'img': 'assets/image/home/banner-image/youth-helpline.jpg'
+      },
+    ]
   }
 
 }
